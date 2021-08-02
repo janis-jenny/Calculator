620aa9a225a823a805320298674729108950644f
janis-jenny <pcardenasvm@gmail.com>
2021-08-02 14:50:28 -0500

    Add calculate results without operate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,24 @@
 const calculate = (data, btnName) => {
   const { total, next, operation } = data;
+
+  // Multiply by -1, the lastly typed number
+  if (btnName === '+/-') {
+    if (!next) return { total: (total * -1).toString(), next, operation };
+    if (!total) return { total, next: (next * -1).toString(), operation };
+  }
+
+  // Add a dot at the end of the recently typed number
+  if (btnName === '.') {
+    if (!next) return { total: `${total}${btnName}`, next, operation };
+    if (!total) return { total, next: `${next}${btnName}`, operation };
+  }
+
+  // Reset all values if AC is clicked or the result/total is an error
+  if (btnName === 'AC' || total === 'Error') {
+   return { total: null, next: null, operation: null };
+  }
+
+  return { total, next, operation };
 };
+
+export default calculate;
