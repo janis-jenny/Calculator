ff0300846d11ad4fe30046ec7739b1f1871ea775
janis-jenny <pcardenasvm@gmail.com>
2021-08-03 17:43:59 -0500

    Refactor App as a class component and initialize its state

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,34 @@
+import { Component } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Calculator
-        </h1>
-      </header>
-      <>
-        <Display />
-        <ButtonPanel />
-      </>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Calculator
+          </h1>
+        </header>
+        <>
+          <Display />
+          <ButtonPanel />
+        </>
+      </div>
+    );
+  }
 }
 
 export default App;
