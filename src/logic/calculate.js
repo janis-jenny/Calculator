a00571d3833e0f16829bcf4818392e8bc628ad8f
janis-jenny <pcardenasvm@gmail.com>
2021-08-07 20:08:11 -0500

    Move the if statements to the switch statements in calculate function

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,26 +1,8 @@
+/* eslint-disable max-len */
 import operate from './operate';
 
 const calculate = (data, btnName) => {
   const { total, next, operation } = data;
-
-  // Multiply by -1, the lastly typed number
-  if (btnName === '+/-') {
-    if (!next) return { total: (total * -1).toString(), next, operation };
-    if (!total) return { total, next: (next * -1).toString(), operation };
-  }
-
-  // Add a dot at the end of the recently typed number
-  if (btnName === '.') {
-    if (!next && !total) return { total, next: `${0}${btnName}`, operation };
-    if (!next) return { total: `${total}${btnName}`, next, operation };
-    if (!total) return { total, next: `${next}${btnName}`, operation };
-    if (operation && total && (next !== '0' || next === '0')) return { total, next: `${next}${btnName}`, operation };
-  }
-
-  // Reset all values if AC is clicked or the result/total is an error
-  if (btnName === 'AC' || total === 'Error') {
-    return { total: null, next: null, operation: null };
-  }
 
   switch (btnName) {
     case '1':
@@ -62,6 +44,18 @@ const calculate = (data, btnName) => {
       const result = operate(total, next, operation);
       return { total: result, next: null, operation: null };
     }
+    case '+/-':
+      if (!next) return { total: (total * -1).toString(), next, operation };
+      if (!total) return { total, next: (next * -1).toString(), operation };
+      break;
+    case 'AC':
+      return { total: null, next: null, operation: null };
+    case '.':
+      if (!next && !total) return { total, next: `${0}${btnName}`, operation };
+      if (!next) return { total: `${total}${btnName}`, next, operation };
+      if (!total) return { total, next: `${next}${btnName}`, operation };
+      if (operation && total && (next !== '0' || next === '0')) return { total, next: `${next}${btnName}`, operation };
+      break;
     default:
       return data;
   }
