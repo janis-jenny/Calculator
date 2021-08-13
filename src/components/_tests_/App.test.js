f61890db88973afe7822eaf697e0af801f87129a
janis-jenny <pcardenasvm@gmail.com>
2021-08-13 17:42:52 -0500

    Set up and try expectations tests with jest

---
+++ src/components/_tests_/App.test.js
@@ -0,0 +1,15 @@
+/* import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App'; */
+/* import Home from '../Home';
+import Calculator from '../Calculator';
+import Quote from '../Quote'; */
+describe('App Component', () => {
+  it('passing test', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('failing test', () => {
+    expect(false).toBeFalsy();
+  });
+});
