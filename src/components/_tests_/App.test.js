f894ece7350649d929c4e17e7d80e6b24a68f895
janis-jenny <pcardenasvm@gmail.com>
2021-08-15 20:33:06 -0500

    Add quote and app tests

---
+++ src/components/_tests_/App.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import App from '../App';
+
+describe('App Component', () => {
+  const appComponent = (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  it('Should render the App component successfully', () => {
+    render(appComponent);
+  });
+
+  it('Should display the navbar', () => {
+    const { getByText } = render(appComponent);
+    getByText('Home');
+    getByText('Calculator');
+    getByText('Quotes');
+  });
+
+  it('Should navigate to the calculator page if "Calculator" tab is clicked', () => {
+    const { getByText, getAllByRole } = render(appComponent);
+    const btn = getByText('Calculator');
+    fireEvent.click(btn);
+    const calculatorBtns = getAllByRole('button');
+    expect(calculatorBtns.length).toBe(19);
+  });
+
+  it('Should navigate to the quotes page if "Quotes" tab is clicked', () => {
+    const { getByText } = render(appComponent);
+    const btn = getByText('Quotes');
+    fireEvent.click(btn);
+    getByText('Keep going forward because success will come');
+  });
+
+  it('Should render the Calculator page correctly', () => {
+    const tree = renderer.create(<App />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
